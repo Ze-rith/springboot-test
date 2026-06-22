03837a6f0abb0dbe0bad566397fea444995ae69b
김승우 <me.secur1ty.dev@gmail.com>
2026-06-22 14:56:36 +0900

    feat: 기초 세팅

---
+++ src/main/java/com/dgsw/liblong/LiblongApplication.java
@@ -0,0 +1,13 @@
+package com.dgsw.liblong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiblongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiblongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dgsw/liblong/LiblongApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dgsw.liblong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiblongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
